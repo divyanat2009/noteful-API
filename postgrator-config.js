a2b094143634eac903422002996961f1ca692f45
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-17 18:20:14 -0600

    PRODUCTION_DB_URL

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,9 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
-"migrationsDirectory": "migrations",
+    "migrationsDirectory": "migrations",
     "driver": "pg",
-    "host": process.env.MIGRATION_DB_HOST || 'localhost',
-    "port": process.env.MIGRATION_DB_PORT || '5433',
-    "database": process.env.MIGRATION_DB_NAME || 'noteful',
-    "username": process.env.MIGRATION_DB_USER || 'postgres',
-    "password": process.env.MIGRATION_DB_PASS    
-}        
+    "connectionString": process.env.PRODUCTION_DATABASE_URL,
+    "ssl": !!process.env.SSL,
+}
