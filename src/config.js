5ed9ff2768e61a931bd7a0ae11af4bf092cd1a30
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-21 14:28:12 -0600

    msg

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost:5433/noteful'
+  DB_URL: process.env.DATABASE_URL || 'postgresql://postgres:bragada013078@localhost:5433/noteful'
+
 };
