9bfdc172e91932125dff9a18ddf010de39360a64
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-21 10:47:43 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -10,13 +10,12 @@ const notesRouter = require('./notes/notes-router');
 
 
 const app = express();
-
+app.use(cors());
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
 
 app.use('/api/folders', foldersRouter);
 app.use('/api/notes', notesRouter);
