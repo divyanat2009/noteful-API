bd99d6487cef645ad679b0f871a232bd4db88c8f
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-23 11:21:09 -0600

    changed port back to 5432

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost:5433/noteful1',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/noteful-test',
-  DB_PORT: process.env.DB_PORT || 5433,
+  DB_PORT: process.env.DB_PORT || 5432,
      
 };
