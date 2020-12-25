8b0b5a12d6b298e7cdce8401ffa0078120514baf
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-24 21:19:07 -0600

    commit

--- src/config.js
+++ src/config.js
@@ -3,6 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/noteful',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/noteful-test',
-  DB_PORT: process.env.DB_PORT || 5432,
-     
+      
 };
