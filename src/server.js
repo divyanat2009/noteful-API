b476bca4035e90819e916e1abde43391e2a36328
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-29 11:29:33 -0600

    test

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const { PORT, DATABASE_URL } = require('./config');
 
 const db  = knex({
   client: 'pg',
-  connection: DATABASE_URL,  
+  connection: 'postgres://ywzfulztpkakhk:b1bf24603849950e5ead934083fee44b3af34ed6eb9ed058b50ae17bab9bcc79@ec2-50-19-26-235.compute-1.amazonaws.com:5432/dcprlf6nbc330i?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory',  
 });
 
 app.set('db', db);
